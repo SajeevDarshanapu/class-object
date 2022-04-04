fd4aea820b3589bb36e936a040e31cf73a108718
SajeevDarshanapu <67383521+SajeevDarshanapu@users.noreply.github.com>
2022-04-04 15:04:11 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿class car
+{
+    string colour = "Blacku";
+    string name = "BMW";
+    int model = 2022;
+
+    static void Main(String[]args)
+    {
+        car krish = new car();
+        Console.WriteLine("Colour of my BMW is "+ krish.colour);
+        Console.WriteLine("Name of my car is "+ krish.name);
+        Console.WriteLine("Model of my BMW is " +krish.model);
+    }
+}
